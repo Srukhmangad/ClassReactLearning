c24d1438bbe8b5f91ccdddb241e1a2543ba34c71
Srukhmangad <sumitrukhmangad@outlook.com>
2023-05-21 19:04:33 +0530

    WIP: adding bootstrap header

---
+++ client/src/components/Header.js
@@ -0,0 +1,11 @@
+import react from 'react'
+import './Header.css'
+
+function Header(){
+    return(
+        <div>
+            <h1 className="heading">Header</h1>
+        </div>
+    )
+}
+export default Header
