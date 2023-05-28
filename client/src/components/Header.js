f76f3b3fb04c2bab96eeb0014655bf1c79645030
Srukhmangad <sumitrukhmangad@outlook.com>
2023-05-28 18:29:42 +0530

    WIP: conditional rendering exercise

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -8,14 +8,15 @@ function Header({name, setName,showTitlePage,setShowTitlePage}){
         if(name=='HeroVired'){
             setName('Hero')
         setShowTitlePage(false)}
-        else{setName('HeroVired')}
+        else{setName('HeroVired') 
+        setShowTitlePage(true)}
       }
 
     return(
         <div>
             <div>Welcome to {name}</div>
+            <div style={{color:'brown'}}>This is Conditionally rendered Title{showTitlePage? <Title/>:null}</div>
             <button onClick={HandleClick}>Click Here</button>
-            <div>This is Conditionally rendered Title{showTitlePage? <Title/>:null}</div>
         </div>
     )
 }
